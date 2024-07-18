28ca0c338fccb9a69a24bb8a5a0274a6a6ca0ad8
Bruno Navarro <brunonavarro34@gmail.com>
2024-07-18 18:43:43 -0500

    full documentation español

--- docs/styles/script.js
+++ docs/styles/script.js
@@ -1,17 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const codeElement = document.getElementById('code');
-    highlightCode(codeElement);
+document.addEventListener("DOMContentLoaded", (event) => {
+    document.querySelectorAll('pre code').forEach((block) => {
+        hljs.highlightBlock(block);
+    });
 });
-
-function highlightCode(element) {
-    const code = element.textContent;
-    const highlightedCode = code
-        .replace(/(fun|val)/g, '<span class="token keyword">$1</span>')
-        .replace(/(\w+)(?=\()/g, '<span class="token function">$1</span>')
-        .replace(/"(.*?)"/g, '<span class="token string">"$1"</span>')
-        .replace(/\/\/(.*?)(\n|$)/g, '<span class="token comment">//$1</span>$2')
-        .replace(/(\+|\-|\/|\*|=)/g, '<span class="token operator">$1</span>')
-        .replace(/([{}()\[\].,;])/g, '<span class="token punctuation">$1</span>');
-
-    element.innerHTML = highlightedCode;
-}
