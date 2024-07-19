c9f57764d5f55df2fd08685038e347e2afc4493f
Bruno Navarro <brunonavarro34@gmail.com>
2024-07-18 19:03:09 -0500

    full documentation español

--- docs/styles/script.js
+++ docs/styles/script.js
@@ -1,22 +1,9 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    document.querySelectorAll('pre code').forEach((block) => {
-        hljs.highlightBlock(block);
-        addLineNumbers(block);
-    });
-});
-
-function addLineNumbers(code) {
-    const lines = code.innerHTML.split('\n');
-    code.innerHTML = lines.map((line, index) => {
-        return `<span class="line">${line}</span>`;
-    }).join('\n');
-}
-
 function copyToClipboard() {
-    const code = document.querySelector('pre code').textContent;
+    const code = document.querySelector('pre code').innerText;
     navigator.clipboard.writeText(code).then(() => {
         alert('Código copiado al portapapeles');
     }).catch(err => {
         alert('Error al copiar el código: ', err);
     });
 }
+
