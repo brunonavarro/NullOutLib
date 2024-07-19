8e44a8895b27dca1b114ccf67cbcd6ae036e2a6c
Bruno Navarro <brunonavarro34@gmail.com>
2024-07-18 22:53:33 -0500

    full documentation español

--- docs/styles/script.js
+++ docs/styles/script.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    document.querySelectorAll('pre code').forEach((block) => {
-        hljs.highlightBlock(block);
-        addLineNumbers(block);
-    });
-});
-
-function addLineNumbers(code) {
-    const lines = code.innerHTML.split('\n');
-    code.innerHTML = lines.map((line, index) => {
-        return `<span class="line">${line}</span>`;
-    }).join('\n');
-}
+//document.addEventListener("DOMContentLoaded", (event) => {
+//    document.querySelectorAll('pre code').forEach((block) => {
+//        hljs.highlightBlock(block);
+//        addLineNumbers(block);
+//    });
+//});
+//
+//function addLineNumbers(code) {
+//    const lines = code.innerHTML.split('\n');
+//    code.innerHTML = lines.map((line, index) => {
+//        return `<span class="line">${line}</span>`;
+//    }).join('\n');
+//}
 
 function copyToClipboard() {
     const code = document.querySelector('pre code').innerText;
